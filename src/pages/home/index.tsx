bc0d1b4e1b05f9064f414481c4b3d928bebdc484
EtherCD <ethernetcoder@gmail.com>
2025-11-21 20:27:01 +0200

    New background. And design

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -5,6 +5,7 @@ import { Translation, useTranslation } from "i18nano";
 import styles from "./index.module.css";
 import { MysticalAgriexpansion } from "../../components/ProjectsCard/MysticalAgriexpansion";
 import { Footer } from "../../components/Footer";
+import { LanguageSelect } from "../../components/LanguageSelect";
 
 export const HomePage = () => {
 	const i18n = useTranslation();
@@ -15,48 +16,48 @@ export const HomePage = () => {
 					<h1>
 						<Translation path={"h1.always"} />
 					</h1>
+					<h1 class={styles.enjoy}>
+						<Translation path={"h1.enjoy"} />
+					</h1>
+					<div class={styles.contacts}>
+						<LanguageSelect />
+						<a href="https://github.com/EtherCD" target="_blank">
+							<img src="/icons/github.svg" alt="" />
+						</a>
+						<a href="https://discord.com/users/930091767842353212" target="_blank">
+							<img src="/icons/discord.svg" alt="" />
+						</a>
+					</div>
 				</div>
-				<DiscordUserCard
-					bannerImage="/card/banner.png"
-					bannerColor="#3b6e8d"
-					username="EtherCD"
-					userId="ethercd"
-					avatar="/card/avatar.png"
-					userLabel="DEV"
-					memberSince="Aug 30, 2007"
-					nitro={{
-						accent: "#886636",
-						additional: "#f3ca88",
-					}}
-					roles={[
-						{ name: "TypeScript", color: "#3178C6" },
-						{ name: "Java", color: "#f89820" },
-						{ name: "Zig", color: "#F7A41D" },
-					]}
-					badges={[
-						{
-							iconUrl: "https://raw.githubusercontent.com/pixelate-it/pixelbattle-frontend/1bf9e7ce41780ddbc31afca2871579f3270652ae/public/images/meta/favicon.svg",
-							tooltip: "Pixelate It! Dev",
-						},
-					]}
-					buttons={[
-						{
-							text: "My Github",
-							url: "https://github.com/EtherCD",
-							iconUrl: "/icons/github.svg",
-						},
-					]}
-					thinks={i18n("card.thinks")}
-				>
-					<Translation path={"card.about"} />
-				</DiscordUserCard>
-				<p class={styles.links}>
-					-{" "}
-					<a href="#projects">
-						<Translation path={"h1.list.projects"} />
-					</a>
-					, <Translation path={"h1.list.contacts"} /> -
-				</p>
+				<div class={styles.card}>
+					<DiscordUserCard
+						bannerImage="/card/banner.png"
+						bannerColor="#3b6e8d"
+						username="EtherCD"
+						userId="ethercd"
+						avatar="/card/avatar.png"
+						userLabel="DEV"
+						memberSince="Aug 30, 2007"
+						nitro={{
+							accent: "#886636",
+							additional: "#f3ca88",
+						}}
+						roles={[
+							{ name: "TypeScript", color: "#3178C6" },
+							{ name: "Java", color: "#f89820" },
+							{ name: "Zig", color: "#F7A41D" },
+						]}
+						badges={[
+							{
+								iconUrl: "https://raw.githubusercontent.com/pixelate-it/pixelbattle-frontend/1bf9e7ce41780ddbc31afca2871579f3270652ae/public/images/meta/favicon.svg",
+								tooltip: "Pixelate It! Dev",
+							},
+						]}
+						thinks={i18n("card.thinks")}
+					>
+						<Translation path={"card.about"} />
+					</DiscordUserCard>
+				</div>
 			</div>
 			<h1 id="projects" class={styles.projects}>
 				<Translation path={"h1.projects"} />
