4c346c9123c8bd221a3fe03533d5d1008e29af49
EtherCD <ethernetcoder@gmail.com>
2025-11-21 18:14:00 +0200

    New background + fixes for mobile view

--- src/components/Background/Stars.ts
+++ src/components/Background/Stars.ts
@@ -3,6 +3,8 @@ interface Particle {
 	y: number;
 	r: number;
 	t: number;
+	d: number;
+	s: number;
 }
 
 function random(min: number, max: number) {
@@ -10,7 +12,7 @@ function random(min: number, max: number) {
 }
 
 export class StarEffect {
-	maxParts = 100;
+	maxParts = 40;
 	particles: Particle[] = [];
 	delay = random(300, 500);
 	time = Date.now();
@@ -19,8 +21,10 @@ export class StarEffect {
 			this.particles.push({
 				x: random(0, width),
 				y: random(0, height),
-				r: random(1.5, 3),
-				t: random(0, 1000),
+				r: random(5, 10),
+				t: random(1000, 5000),
+				d: random(-Math.PI * 2, Math.PI * 2),
+				s: random(0, 1000),
 			});
 		}
 	}
@@ -32,18 +36,36 @@ export class StarEffect {
 		for (var b = 0; b < this.particles.length; b++) {
 			var p = this.particles[b];
 
+			ctx.globalAlpha = 1;
+			p.s -= delta;
+			const step = Math.PI / 5;
+			ctx.save();
+			ctx.translate(p.x, p.y);
+			ctx.rotate(p.d);
+			const inset = 3;
 			ctx.beginPath();
-			ctx.fillStyle = "rgba(255,255,255,0.6)";
-			ctx.globalAlpha = p.t / 3000;
-			ctx.arc(p.x, p.y, p.r, 0, Math.PI * 2);
-			ctx.fill();
+			let r = p.s > 0 ? (-(p.s / 1000) + 1) * p.r : p.r;
+			if (p.t < 1000) r = (p.t / 1000) * p.r;
+
+			ctx.moveTo(0, -p.r);
+
+			for (let i = 0; i < 5; i++) {
+				ctx.lineTo(Math.cos(step * (2 * i + 1)) * r * inset, Math.sin(step * (2 * i + 1)) * r * inset);
+				ctx.lineTo(Math.cos(step * (2 * i + 2)) * r, Math.sin(step * (2 * i + 2)) * r);
+			}
+
+			ctx.fillStyle = "rgba(213,196,161,0.3)";
 			ctx.closePath();
+			ctx.fill();
+			ctx.restore();
 
 			p.t -= delta;
 			if (p.t <= 0) {
 				p.y = random(0, height);
 				p.x = random(0, width);
-				p.t = random(1000, 3000);
+				p.t = random(2000, 10000);
+				p.d = random(0, Math.PI * 2);
+				p.s = 1000;
 			}
 		}
 	}
