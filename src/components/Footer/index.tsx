bf40dccbae743da6bc707da125947b059f801df9
EtherCD <ethernetcoder@gmail.com>
2025-11-23 10:41:33 +0200

    New backgrounds for effects/seasons.

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,21 +1,28 @@
 import styles from "./index.module.css";
 
+const email = "ethercd@proton.me";
+
 export const Footer = () => {
-    return (
-        <footer class={styles.footer}>
-            <h1 id="contacts">@EtherCD - 2025</h1>
-            <div>
-                <p>Contacts - </p>
-                <a href="https://github.com/EtherCD" target="_blank">
-                    <img src="/icons/github.svg" alt="" />
-                </a>
-                <a
-                    href="https://discord.com/users/930091767842353212"
-                    target="_blank"
-                >
-                    <img src="/icons/discord.svg" alt="" />
-                </a>
-            </div>
-        </footer>
-    );
+	const copyToClipboard = async () => {
+		await navigator.clipboard.writeText(email);
+	};
+
+	return (
+		<footer class={styles.footer}>
+			<h1 id="contacts">@EtherCD - 2025</h1>
+			<div>
+				<p>Contacts - </p>
+				<a href="https://github.com/EtherCD" target="_blank">
+					<img src="/icons/github.svg" alt="" />
+				</a>
+				<a href="https://discord.com/users/930091767842353212" target="_blank">
+					<img src="/icons/discord.svg" alt="" />
+				</a>
+				<div class={styles["success-icon"]} onClick={copyToClipboard}>
+					<img src="/icons/email.svg" alt="email" />
+					<div class={`${styles.tooltip} ${styles["success-up"]}`}>{email}</div>
+				</div>
+			</div>
+		</footer>
+	);
 };
